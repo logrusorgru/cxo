19a0990703c7da519082f7586dcc26e77065e564
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2018-05-08 02:21:18 +0300

    data/idx/redis range_roots.lua  implemented

--- data/idx/redis/lua/range_roots.lua
+++ data/idx/redis/lua/range_roots.lua
@@ -2,27 +2,28 @@
 -- range_roots.lua
 --
 
--- in:  feed, head, min, max
+-- in:  feed, head, start, scan_by, reverse
 -- out: {has_feed, has_head, {seqs}}
 
-local hex  = ARGV[1];
-local head = ARGV[2];
-local min  = ARGV[3];
-local max  = ARGV[4];
+local hex     = ARGV[1];
+local head    = ARGV[2];
+local start   = ARGV[3];
+local scan_by = ARGV[4];
+local reverse = ARGV[5];
 
 -- feed
 local feed = 'idx:feed:' .. hex;
 
 local has_feed = 0;
 local has_head = 0;
-local seqs;
+local seqs     = {};
 
 -- has feed
 has_feed = redis.call('EXISTS', feed);
 
 -- has not
 if has_feed == 0 then
-	return {has_feed, has_head, {}};
+	return {has_feed, has_head, seqs};
 end
 
 -- has head
@@ -30,21 +31,32 @@ has_head = redis.call('HEXISTS', feed, head);
 
 -- has not
 if has_head == 0 then
-	return {has_feed, has_head, {}};
+	return {has_feed, has_head, seqs};
 end
 
--- key for ZRANGEBYSCORE (idx:feed:head)
+-- key for Z[REV]RANGEBYSCORE (idx:feed:head)
 local zkey    = 'idx:' .. hex .. ':' .. head;
-local command = 'ZRANGEBYSCORE';
+
+local command;
+local min;
+local max;
 
 -- asc or desc
-if tonumber(min) > tonumber(max) then
+if reverse == '0' then
+	-- direct
+	command = 'ZRANGEBYSCORE';
+	min     = start;
+	max     = '+inf';
+else
+	-- inverse
 	command = 'ZREVRANGEBYSCORE';
+	min     = start;
+	max     = '-inf';
 end
 
 --
-seqs = redis.call(command, zkey,
+local seqs = redis.call(command, zkey,
 	min, max,
-	'WITHSCORES');
+	'LIMIT', 0, scan_by);
 
 return {has_feed, has_head, seqs};
